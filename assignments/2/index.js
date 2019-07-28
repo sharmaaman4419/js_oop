d8af8cdc37655c59c59cbb0325e452cc11958a6b
sharmaaman4419 <sharmaaman4419@gmail.com>
2019-07-28 17:52:35 +0530

    sol-added

--- assignments/2/index.js
+++ assignments/2/index.js
@@ -5,17 +5,18 @@
 /*** CHALLENGE 1 of 1 ***/
 
 function makePerson(name, age) {
-	// add code here
-
-
+  let obj = {};
+  obj.name = name;
+  obj.age = age;
+  return obj;
 }
 
 var vicky = makePerson('Vicky', 24);
 
 
 // /********* Uncomment these lines to test your work! *********/
-// console.log(vicky.name); // -> Logs 'Vicky'
-// console.log(vicky.age); // -> Logs 24
+ console.log(vicky.name);  -> Logs 'Vicky'
+ console.log(vicky.age);  -> Logs 24
 
 
 
